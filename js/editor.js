ee811b17e754d47af78d1efcbf2ad7db444ffd96
Rizwan Sharif <rizwan6532@gmail.com>
2012-07-31 12:13:19 -0700

    Added iFrame

--- js/editor.js
+++ js/editor.js
@@ -1,68 +1,6 @@
 $(document).ready(function(){
 
 	var parentDiv = $("#main");
-	$('body').css({
-		"margin": "0px"
-	});
-	
-	$('body').css({
-		"background-color": "#EBEBEB"
-	});
-	
-	var controlsDiv;
-	(function createControlDiv(){
-		controlsDiv = $("<div/>",{
-			id : "controlsDiv"
-		}).appendTo(parentDiv);
-		
-		controlsDiv.css({
-			
-			"background-color":"whiteSmoke",
-			"padding-top" : "10px",
-			"padding-bottom" : "10px",
-			"border-top" : "1px solid #E5E5E5",
-			"border-bottom" : "1px solid #E5E5E5",
-			"min-height" : "22px"
-			
-		});
-		controlsDiv.parent = parentDiv;
-	})();
-	
-	var contentContainerDiv ;
-	(function createContentDiv(){
-		contentContainerDiv = $("<div/>",{
-			id : "contentContainerDiv"
-		}).appendTo(parentDiv);
-		contentContainerDiv.css({
-			"padding-top" : "10px",
-			"padding-bottom" : "10px",
-			"min-height" : "100px",
-			"text-align" : "center"	
-		});
-		
-	})();
-	
-	var contentDiv;
-	(function(){
-		contentDiv=$("<div/>",{
-			id : "contentDiv"
-		}).appendTo(contentContainerDiv);
-		
-		contentDiv.css({
-			"background-color" : "white",
-			"width" : "818px",
-			"height" : "100%",
-			"margin" : "auto",
-			"border" : "1px solid #DADADA",
-			"-moz-box-shadow" : "0 0 4px rgba(0, 0, 0, 0.1)",
-   			"-webkit-box-shadow" : "0 0 4px rgba(0, 0, 0, 0.1)",
-   			"box-shadow" :  "0 0 4px rgba(0, 0, 0, 0.1)"  //TODO Adjust for IE
-   			
-		});
-		
-	})();
-	
-	contentDiv.parent = parentDiv;
 	
 	var enabledPlugins = [new TextColor()];
 	(function createControls( controlsDiv ){
