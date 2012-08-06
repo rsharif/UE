9bbc56b1fdd1bda9e64e8ea9e9a87f984f0085aa
Rizwan Sharif <rizwan6532@gmail.com>
2012-08-06 10:00:00 -0700

    Fixed Firefox line breaking issue

--- js/iframe/main.js
+++ js/iframe/main.js
@@ -1,14 +1,22 @@
 function load(){
 	
 	var ua = $.browser;
-	
-	$(".contentContainer").keypress(function(event){
+	if (ua.mozilla) {
+//		var selection=window.getSelection();
+//		var contentPage = $("#contengPage");
+//		var newRange = document.createRange();
+//		newRange.setStart($(contentPage,0);
+//		newRange.setEnd(contentPage,0);
+//		$("#contentPage").addRange(newRange);
+	}
+	$(".contentPage").keypress(function(event){
 		
 		/* find cursor position */
 		var selection=window.getSelection();
 		var range=selection.getRangeAt(0);
 		var start = range.startOffset;
 		var end = range.endOffset;
+		var anchorNode = selection.anchorNode;
 		
 		var pressedKeyCode = event.which;
 		if(pressedKeyCode < 90 || pressedKeyCode > 124 ){
@@ -27,25 +35,57 @@ function load(){
 			 */
 			var brTags = $(selection.anchorNode).find("br");
 			var noOfBr = brTags.length;
-			for(var i = 0 ; i < noOfBr ; i++){
-				if(i >= noOfBr -1 && !(ua.mozilla && noOfBr == 1 )){ //mozilla only inserts one br on first entery key which we need to keep
-					 $(brTags[i]).remove();
+			 //mozilla only inserts one br on first entery key which we need to keep
+			if (ua.mozilla) {
+				 
+				if( $(brTags[ noOfBr -1 ]).attr('type') === '_moz') $(brTags[noOfBr -1]).remove();
+
+				if (start == anchorNode.childNodes.length - 2 || start < 2 || $(brTags[noOfBr - 1]).attr('type') === '_moz') {
+				
+					$(anchorNode).append(textNode);
+					selection.removeAllRanges();
+					/* create new Range and set it in the newly created text ndoe just after the character typed */
+					var newRange = document.createRange();
+					newRange.setStart(textNode, 1);
+					newRange.setEnd(textNode, 1);
+					selection.removeAllRanges();
+					selection.addRange(newRange);
+				}
+				else {
+					/* This block just handles the insertion for first character in a new line which is created by splitting the previous line 
+					 * i.e by pressing enter in the line which already contains some text
+					 */
+					var nextChild = anchorNode.childNodes[start];
+					textNode = document.createTextNode(urduCharacter + nextChild.wholeText);
+					$($(anchorNode).contents()[start]).remove();
+					$($(anchorNode).contents()[start-1]).after(textNode);
+					var newRange = document.createRange();
+					newRange.setStart(textNode, 1);
+					newRange.setEnd(textNode, 1);
+					selection.removeAllRanges();
+					selection.addRange(newRange);
 				}
 			}
-			
-			$(selection.anchorNode).append(textNode);
-			selection.removeAllRanges();
-			/* create new Range and set it in the newly created text ndoe just after the character typed */
-			var newRange = document.createRange();
-			newRange.setStart(textNode,1);
-			newRange.setEnd(textNode,1);
-			selection.addRange(newRange);
+			/*for mozilla only delete the br tags which has attribute type=_moz.because thats the last br tag. 
+			* if you break a typed line from middle , mozilla does not insert any extra br tag so no br tag should be removed
+			*/
+			if (ua.safari || ua.webkit) {
+				$(brTags[noOfBr - 1]).remove();	
+				$(anchorNode).append(textNode);
+				selection.removeAllRanges();
+				/* create new Range and set it in the newly created text ndoe just after the character typed */
+				var newRange = document.createRange();
+				newRange.setStart(textNode, 1);
+				newRange.setEnd(textNode, 1);
+				selection.removeAllRanges();
+				selection.addRange(newRange);
+			}
 			
 		}
 		else 
 		{
 			/* Get the current text inside the pseudo textarea */
-			var originalText = selection.anchorNode.nodeValue 	;
+			var originalText = anchorNode.nodeValue 	;
 			/* Get text before the cursor */
 			var firstHalf = originalText && originalText.slice(0,start);
 			firstHalf = (firstHalf === null ) ? "" : firstHalf;
@@ -58,8 +98,8 @@ function load(){
 			selection.anchorNode.nodeValue = newStr;
 			
 			var newRange = document.createRange();
-			newRange.setStart(selection.anchorNode, start +1);
-			range.setEnd(selection.anchorNode, end+1);
+			newRange.setStart(anchorNode, start +1);
+			range.setEnd(anchorNode, end+1);
 			selection.removeAllRanges();
 			selection.addRange(newRange);
 		}
